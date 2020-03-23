91e9e0f40a11ef0c6c24190bec36d701d758f590
Andrew Hicks <hicks.andrew@gmail.com>
2020-03-23 13:42:24 -0600

    updates

--- DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Filtering rows.sql
+++ DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Filtering rows.sql
@@ -1,1 +1,5 @@
-Filtering rows
+-- Title: Filtering rows
+
+setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Introduction to SQL/Datasets")
+
+-- Simple filtering of numeric values
--- DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Selecting columns.sql
+++ DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Selecting columns.sql
@@ -1,1 +1,78 @@
-Selecting columns
+-- Title: Selecting columns
+
+setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Introduction to SQL/Datasets")
+
+-- Onboarding | Query Result
+SELECT name FROM people;
+
+-- Onboarding | Errors
+-- Try running me!
+SELECT 'DataCamp <3 SQL'
+AS result;
+
+-- Onboarding | Bullet Exercises
+SELECT 'SQL'
+AS result;
+
+SELECT 'SQL is'
+AS result;
+
+SELECT 'SQL is cool!'
+AS result;
+
+-- SELECTing single columns
+SELECT title
+FROM films;
+
+SELECT release_year
+FROM films;
+    
+SELECT name
+FROM people;
+
+-- SELECTing multiple columns
+SELECT title
+FROM films;
+
+SELECT title, release_year
+FROM films;
+
+SELECT title, release_year, country
+FROM films;
+
+SELECT *
+FROM films;
+
+-- SELECT DISTINCT
+SELECT DISTINCT country
+FROM films;
+
+SELECT DISTINCT certification
+FROM films;
+
+SELECT DISTINCT role
+FROM roles;
+
+-- Learning to COUNT
+SELECT COUNT(*)
+FROM reviews;
+
+-- Practice with COUNT
+SELECT count(DISTINCT id)
+FROM people;
+
+SELECT COUNT(birthdate)
+FROM people;
+
+SELECT COUNT(DISTINCT birthdate)
+FROM people;
+
+SELECT COUNT(DISTINCT language)
+FROM films;
+
+SELECT COUNT(DISTINCT country)
+FROM films;
+
+
+
+
