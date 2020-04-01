146af2a37ed4ff470f49a76218a1244ab4a67c9b
Andrew Hicks <hicks.andrew@gmail.com>
2020-03-31 22:58:59 -0600

    Update

--- DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Sorting and grouping.sql
+++ DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Sorting and grouping.sql
@@ -23,3 +23,15 @@ ORDER BY duration;
 SELECT title, gross FROM films
 WHERE title LIKE 'M%'
 ORDER BY title;
+
+-- Sorting single columns (DESC)
+SELECT imdb_score, film_id FROM reviews
+ORDER BY imdb_score DESC;
+
+SELECT title FROM films
+ORDER BY title DESC;
+
+SELECT title, duration FROM films
+ORDER BY duration DESC;
+
+-- Sorting multiple columns
