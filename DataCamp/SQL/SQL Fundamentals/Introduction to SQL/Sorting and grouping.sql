1ed426e52f67d6c93bedba7a3ce88b14a953a82c
Andrew Hicks <hicks.andrew@gmail.com>
2020-03-31 18:53:22 -0600

    Update

--- DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Sorting and grouping.sql
+++ DataCamp/SQL/SQL Fundamentals/Introduction to SQL/Sorting and grouping.sql
@@ -2,4 +2,24 @@ Sorting and grouping
 
 setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Introduction to SQL/Datasets")
 
--- ORDER BY
+-- Sorting single columns
+SELECT name FROM people
+ORDER BY name;
+
+SELECT name FROM people
+ORDER BY name;
+
+SELECT birthdate, name FROM people
+ORDER BY birthdate;
+
+-- Sorting single columns (2)
+SELECT title FROM films
+WHERE (release_year = 2000 OR release_year = 2012);
+
+SELECT * FROM films
+WHERE release_year <> 2015
+ORDER BY duration;
+
+SELECT title, gross FROM films
+WHERE title LIKE 'M%'
+ORDER BY title;
