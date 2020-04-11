61ef54a8ca7dfe2bd433803a3a6e01ce6557a9eb
Andrew Hicks <hicks.andrew@gmail.com>
2020-04-11 15:45:53 -0600

    update

--- DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/1. Introduction to joins.sql
+++ DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/1. Introduction to joins.sql
@@ -2,3 +2,75 @@ Introduction to joins
 
 setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Joining Data in SQL/Datasets")
 
+-- Introduction to INNER JOIN
+SELECT * FROM presidents;
+
+SELECT p1.country, p1.coninent, prime_minister, president
+FROM prime_ministers AS p1
+INNER JOIN prseidents AS p2
+ON p1.country = p2.country;
+
+-- Inner join
+-- Select all columns from cities
+SELECT *
+FROM cities;
+
+SELECT * 
+FROM cities
+  -- 1. Inner join to countries
+  INNER JOIN countries
+    -- 2. Match on the country codes
+    ON cities.country_code = countries.code;
+
+-- 1. Select name fields (with alias) and region 
+SELECT cities.name AS city, countries.name AS country, countries.region
+FROM cities
+  INNER JOIN countries
+    ON cities.country_code = countries.code;
+
+-- Inner join (2)
+-- 3. Select fields with aliases
+SELECT c.code AS country_code, name, year, inflation_rate
+FROM countries AS c
+  -- 1. Join to economies (alias e)
+  INNER JOIN economies AS e
+    -- 2. Match on code
+    ON c.code = e.code;
+
+-- Inner join (3)
+-- 4. Select fields
+SELECT c.code, c.name, c.region, p.year, p.fertility_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join with populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code;
+
+-- 6. Select fields
+SELECT c.code, name, region, e.year, fertility_rate, e.unemployment_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join to populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+  -- 4. Join to economies (as e)
+  INNER JOIN economies AS e
+    -- 5. Match on country code
+    ON c.code = e.code;
+
+-- 6. Select fields
+SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join to populations (as p)
+  INNER JOIN populations AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+  -- 4. Join to economies (as e)
+  INNER JOIN economies AS e
+    -- 5. Match on country code and year
+    ON c.code = e.code AND e.year = p.year;
+
+    -- INNER JOIN via USING
