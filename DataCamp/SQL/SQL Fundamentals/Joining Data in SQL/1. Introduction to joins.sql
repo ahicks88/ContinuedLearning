8a34efca495437544d9774c7c8dcc20309195d00
Andrew Hicks <hicks.andrew@gmail.com>
2020-04-12 17:49:48 -0600

    updates

--- DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/1. Introduction to joins.sql
+++ DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/1. Introduction to joins.sql
@@ -74,3 +74,141 @@ SELECT c.code, name, region, e.year, fertility_rate, unemployment_rate
     ON c.code = e.code AND e.year = p.year;
 
     -- INNER JOIN via USING
+SEECT p1.country, p1.contient, prime_minister, president
+FROM preseidents AS p1
+INNER JOIN prime_minister AS p2
+USING (country);
+
+-- Review inner join using on
+SELECT c.name AS country, l.name AS language
+FROM countries AS c
+  INNER JOIN languages AS l;
+
+-- Inner join with using
+-- 4. Select fields
+SELECT c.name AS country, c.continent, l.name AS language, l.official
+  -- 1. From countries (alias as c)
+  FROM countries AS c
+  -- 2. Join to languages (as l)
+  INNER JOIN languages AS l
+    -- 3. Match using code
+    USING (code);
+
+-- Self-ish joins, just in CASE
+SELECT p1.country AS country1, p2.country AS country2, p1.continent
+FROM prime_mnister AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.continent = p2 continent
+LIMIT 14;
+
+SELECT p1.country AS country1, p2.country AS country2, p1.continent
+FROM prime_ministers AS p1
+INNER JOIN prime_ministers AS p2
+ON p1.continent = p2.continent AND p1.county <> p2.country
+LIMIT 13;
+
+SELECT name, continent, indep_year,
+    CASE WHEN indep_year < 1900 THEN 'before 1900'
+        WHEN indep_year <= 1930 THEN 'between 1900 and 1930'
+        ELSE 'after 1930' END
+        AS indep_year_group
+FROM states
+ORDER BY indep_year_group;
+
+-- Self-Join
+-- 4. Select fields with aliases
+SELECT p1.country_code, p1.size AS size2010, p2.size AS size2015
+-- 1. From populations (alias as p1)
+FROM populations AS p1
+  -- 2. Join to itself (alias as p2)
+  INNER JOIN populations AS p2
+    -- 3. Match on country code
+    ON p1.country_code = p2.country_code;
+
+-- 5. Select fields with aliases
+SELECT p1.country_code, p1.size AS size2010, p2.size AS size2015
+-- 1. From populations (alias as p1)
+FROM populations AS p1
+  -- 2. Join to itself (alias as p2)
+  INNER JOIN populations AS p2
+    -- 3. Match on country code
+    ON p1.country_code = p2.country_code
+        -- 4. and year (with calculation)
+        AND p1.year = p2.year - 5;
+
+  SELECT p1.country_code,
+       p1.size AS size2010, 
+       p2.size AS size2015,
+       -- 1. calculate growth_perc
+       (( p2.size - p1.size)/p1.size * 100.0) AS growth_perc
+-- 2. From populations (alias as p1)
+FROM populations AS p1
+  -- 3. Join to itself (alias as p2)
+  INNER JOIN populations AS p2
+    -- 4. Match on country code
+    ON p1.country_code = p2.country_code
+        -- 5. and year (with calculation)
+        AND p1.year = p2.year - 5;
+
+-- Case when and then
+SELECT name, continent, code, surface_area,
+    -- 1. First case
+    CASE WHEN surface_area > 2000000 THEN 'large'
+        -- 2. Second case
+        WHEN surface_area > 350000 THEN 'medium'
+        -- 3. Else clause + end
+        ELSE 'small' END
+        -- 4. Alias name
+        AS geosize_group
+-- 5. From table
+FROM countries;
+
+-- Inner challenge
+SELECT country_code, size,
+    -- 1. First case
+    CASE WHEN size > 50000000 THEN 'large'
+        -- 2. Second case
+        WHEN size > 1000000 THEN 'medium'
+        -- 3. Else clause + end
+        ELSE 'small' END
+        -- 4. Alias name (popsize_group)
+        AS popsize_group
+-- 5. From table
+FROM populations
+-- 6. Focus on 2015
+WHERE year = 2015;
+
+SELECT country_code, size,
+    CASE WHEN size > 50000000 THEN 'large'
+        WHEN size > 1000000 THEN 'medium'
+        ELSE 'small' END
+        AS popsize_group
+-- 1. Into table
+INTO pop_plus
+FROM populations
+WHERE year = 2015;
+
+-- 2. Select all columns of pop_plus
+SELECT * FROM pop_plus;
+
+SELECT country_code, size,
+  CASE WHEN size > 50000000
+            THEN 'large'
+       WHEN size > 1000000
+            THEN 'medium'
+       ELSE 'small' END
+       AS popsize_group
+INTO pop_plus       
+FROM populations
+WHERE year = 2015;
+
+-- 5. Select fields
+SELECt name, continent, geosize_group, popsize_group
+-- 1. From countries_plus (alias as c)
+FROM countries_plus AS c
+  -- 2. Join to pop_plus (alias as p)
+  INNER JOIN pop_plus AS p
+    -- 3. Match on country code
+    ON c.code = p.country_code
+-- 4. Order the table    
+ORDER BY geosize_group ASC;
--- DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/2. Outer joins and cross joins.sql
+++ DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/2. Outer joins and cross joins.sql
@@ -1,3 +1,75 @@
 Outer joins and cross joins
 
 setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Joining Data in SQL/Datasets")
+
+-- LEFT and RIGHT JOINs
+SELECT p1.country, prime_minister, preseident
+FROM prime_minister AS p1
+LEFT JOIN preseidents AS p2
+ON p1.country = p2.country;
+
+SELECT right_table.id AS R_id,
+       left_table.val AS L_val,
+       right_table.val AS R_val
+FROM left_table
+RIGHT JOIN right_table
+ON left_table.id = right_table.id;
+
+-- Left Join
+-- Select the city name (with alias), the country code,
+-- the country name (with alias), the region,
+-- and the city proper population
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+-- From left table (with alias)
+FROM cities AS c1
+  -- Join to right table (with alias)
+  INNER JOIN countries AS c2
+    -- Match on country code
+    ON c1.country_code = c2.code
+-- Order by descending country code
+ORDER BY code DESC;
+
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+FROM cities AS c1
+  -- 1. Join right table (with alias)
+  LEFT JOIN countries AS c2
+    -- 2. Match on country code
+    ON c1.country_code = c2.code
+-- 3. Order by descending country code
+ORDER BY code DESC;
+
+-- Left Join (2)
+
+/*
+5. Select country name AS country, the country's local name,
+the language name AS language, and
+the percent of the language spoken in the country
+*/
+SELECT c.name AS country, local_name, l.name AS language, percent
+-- 1. From left table (alias as c)
+FROM countries AS c
+  -- 2. Join to right table (alias as l)
+  INNER JOIN languages AS l
+    -- 3. Match on fields
+    ON c.code = l.code
+-- 4. Order by descending country
+ORDER BY country DESC;
+
+/*
+5. Select country name AS country, the country's local name,
+the language name AS language, and
+the percent of the language spoken in the country
+*/
+SELECT c.name AS country, local_name, l.name AS language, percent
+-- 1. From left table (alias as c)
+FROM countries AS c
+  -- 2. Join to right table (alias as l)
+  LEFT JOIN languages AS l
+    -- 3. Match on fields
+    ON c.code = l.code
+-- 4. Order by descending country
+ORDER BY country DESC;
+
+-- Left Join (3)
