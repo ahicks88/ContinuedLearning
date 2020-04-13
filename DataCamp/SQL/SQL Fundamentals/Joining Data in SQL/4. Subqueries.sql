7822a788fde94093e4edd977900f375dc7d996b4
Andrew Hicks <hicks.andrew@gmail.com>
2020-04-12 23:32:01 -0600

    update

--- DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/4. Subqueries.sql
+++ DataCamp/SQL/SQL Fundamentals/Joining Data in SQL/4. Subqueries.sql
@@ -2,3 +2,4 @@ Subqueries
 
 setwd("C:/Users/Andrew Hicks/Documents/Analytics/GitHub/ContinuedLearning/DataCamp/SQL/SQL Fundamental/Joining Data in SQL/Datasets")
 
+-- Subqueries inside WHERE and SELECT clauses
