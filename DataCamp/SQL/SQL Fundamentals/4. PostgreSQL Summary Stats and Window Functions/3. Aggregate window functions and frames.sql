3ab476c2a24330918ae09bb161f434b727affb72
Andrew Hicks <hicks.andrew@gmail.com>
2020-04-18 18:43:42 -0600

    updates

--- DataCamp/SQL/SQL Fundamentals/4. PostgreSQL Summary Stats and Window Functions/3. Aggregate window functions and frames.sql
+++ DataCamp/SQL/SQL Fundamentals/4. PostgreSQL Summary Stats and Window Functions/3. Aggregate window functions and frames.sql
@@ -1,1 +1,295 @@
-Aggregate window functions and frames
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Title: Aggregate window functions and frames
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Aggregate window functions:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Source table
+SELECT
+    Year, COUNT(*) AS Medals
+FROM Summer_Medals
+WHERE
+    Country = 'BRA'
+    AND Medal = 'Gold'
+    AND Year >= 1992
+GROUP BY Year
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Aggregate functions: MAX Result
+WITH Brazil_Medals AS (...)
+
+SELECT MAX(Medals) AS Max_Medals
+FROM Brazil_Medals
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Aggregate functions: SUM Result
+WITH Brazil_Medals AS (...)
+
+SELECT SUM(Medals) AS Total_Medals
+FROM Brazil_Medals
+-------------------------------------------------------------------------------------------------------------------------------------
+-- MAX Window function
+WITH Brazil_Medals AS (...)
+
+SELECT
+    Year, Medals,
+    MAX(Medals)
+        OVER (ORDER BY Year ASC) AS Max_Medals
+FROM Brazil_Medals;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- SUM Window function
+WITH Brazil_Medals AS (...)
+
+SELECT
+    Year, Medals,
+    SUM(Medals)
+        OVER (ORDER BY Year ASC) AS Medals_RT
+FROM Brazil_Models;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Partitioning with aggregate window functions
+WITH Medals AS (...)
+SELECT Year, Country, Medals,
+    SUM(Meals) OVER (...)
+FROM Medals;
+
+WITH Medals AS (...)
+SELECT Year, Country, Medals,
+    SUM(Meals) OVER (PARTITION BY Country ...)
+FROM Medals;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Running totals of athlete medals:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Athlete_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'USA' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT
+  -- Calculate the running total of athlete medals
+  athlete,
+  medals,
+  SUM(medals) OVER (ORDER BY athlete ASC) AS Max_Medals
+FROM Athlete_Medals
+ORDER BY Athlete ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Maximum country medals by year:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('CHN', 'KOR', 'JPN')
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year, Country)
+
+SELECT
+  -- Return the max medals earned so far per country
+  country,
+  year,
+  medals,
+  MAX(medals) OVER (PARTITION BY Country
+                ORDER BY year ASC) AS Max_Medals
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Minimum country medals by year:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH France_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'FRA'
+    AND Medal = 'Gold' AND Year >= 2000
+  GROUP BY Year)
+
+SELECT
+  year,
+  medals,
+  MIN(medals) OVER (ORDER BY year ASC) AS Min_Medals
+FROM France_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Frames:
+-------------------------------------------------------------------------------------------------------------------------------------
+LAST_VALUE(City) OVER(
+    ORDER BY Year ASC  
+    RANGE BETWEEN
+        UNBOUNDED PRECEDING AND
+        UNBOUNDED FOLLOWING
+) AS Last_City;
+-------------------------------------------------------------------------------------------------------------------------------------
+ROWS BETWEEN 3 PRECEDING AND CURRENT ROW;
+ROWS BETWEEN 1 PRECEDING AND 1 FOLLOWING;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Source table
+SELECT
+    Year, COUNT(*) AS Medals
+FROM Summer_Medals
+WHERE
+    Country = 'RUS'
+    AND Medal = 'Gold'
+GROUP BY Year
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- MAX without a frame
+WITH Russia_Model AS (...)
+
+SELECT 
+    Year, Medals,
+    MAX(Medals)
+        OVER (ORDER BY Year ASC) AS Max_Medals
+FROM Russia_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- MAX with a frame
+WITH Russia_Model AS (...)
+
+SELECT 
+    Year, Medals,
+    MAX(Medals)
+        OVER (ORDER BY Year ASC
+            ROWS BETWEEN
+            1 PRECEDING AND CURRENT ROW)
+        AS Max_Medals_Last
+FROM Russia_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Current and following rows
+WITH Russia_Model AS (...)
+
+SELECT 
+    Year, Medals,
+    MAX(Medals)
+        OVER (ORDER BY Year ASC
+            ROWS BETWEEN
+            CURRENT ROW AND 1 FOLLOWING)
+        AS Max_Medals_Next
+FROM Russia_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving maximum of Scandinavian athletes' medals:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Scandinavian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country IN ('DEN', 'NOR', 'FIN', 'SWE', 'ISL')
+    AND Medal = 'Gold'
+  GROUP BY Year)
+
+SELECT
+  -- Select each year's medals
+  year,
+  medals,
+  -- Get the max of the current and next years'  medals
+  MAX(medals) OVER (ORDER BY year ASC
+             ROWS BETWEEN CURRENT ROW
+             AND 1 FOLLOWING) AS Max_Medals
+FROM Scandinavian_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving maximum of Chinese athletes' medals:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Chinese_Medals AS (
+  SELECT
+    Athlete, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'CHN' AND Medal = 'Gold'
+    AND Year >= 2000
+  GROUP BY Athlete)
+
+SELECT
+  -- Select the athletes and the medals they've earned
+  athlete,
+  medals,
+  -- Get the max of the last two and current rows' medals 
+  MAX(medals) OVER (ORDER BY athlete ASC
+            ROWS BETWEEN 2 PRECEDING
+            AND CURRENT ROW) AS Max_Medals
+FROM Chinese_Medals
+ORDER BY Athlete ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving averages and totals:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Source table
+SELECT
+    Year, COUNT(*) AS Medals
+FROM Summer_Medals
+WHERE
+    Country = 'USA'
+    AND Medal = 'Gold'
+    AND Year >= 1980
+GROUP BY Year
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving average
+WITH US_Medals AS (...)
+
+SELECT
+    Year, Medals,
+    AVG(Medals) OVER
+        (ORDER BY Year ASC
+        ROW BETWEEN
+        2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM US_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving total
+WITH US_Medals AS (...)
+
+SELECT
+    Year, Medals,
+    SUM(Medals) OVER
+        (ORDER BY Year ASC
+        ROW BETWEEN
+        2 PRECEDING AND CURRENT ROW) AS Medals_MT
+FROM US_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving average of Russian medals:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Russian_Medals AS (
+  SELECT
+    Year, COUNT(*) AS Medals
+  FROM Summer_Medals
+  WHERE
+    Country = 'RUS'
+    AND Medal = 'Gold'
+    AND Year >= 1980
+  GROUP BY Year)
+
+SELECT
+  Year, Medals,
+  --- Calculate the 3-year moving average of medals earned
+  AVG(medals) OVER
+    (ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Russian_Medals
+ORDER BY Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Moving total of countries' medals:
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Country_Medals AS (
+  SELECT
+    Year, Country, COUNT(*) AS Medals
+  FROM Summer_Medals
+  GROUP BY Year, Country)
+
+SELECT
+  Year, Country, Medals,
+  -- Calculate each country's 3-game moving total
+  SUM(medals) OVER
+    (PARTITION BY country
+     ORDER BY Year ASC
+     ROWS BETWEEN
+     2 PRECEDING AND CURRENT ROW) AS Medals_MA
+FROM Country_Medals
+ORDER BY Country ASC, Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
--- DataCamp/SQL/SQL Fundamentals/4. PostgreSQL Summary Stats and Window Functions/4. Beyond window functions.sql
+++ DataCamp/SQL/SQL Fundamentals/4. PostgreSQL Summary Stats and Window Functions/4. Beyond window functions.sql
@@ -1,1 +1,227 @@
-Beyond window functions
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Title: Beyond window functions
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Pivoting:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Enter CROSSTAB
+CREATE EXTENTION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+    source_sql TEXT
+    $$) AS ct (column_1 DATA_TYPE_1,
+               column_2 DATA_TYPE_2,
+               ...,
+               column_n DATA_TYPE_N);
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Enter CROSSTAB
+
+-- Before:
+SELECT
+    Country, Year, COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+    Country IN ('CHN','RUS','USA')
+    AND Year IN (2008, 2012)
+    AND Medal = 'Gold'
+GROUP BY Country, Year
+ORDER BY Country ASC, Year ASC;
+
+-- After:
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+    SELECT
+        Country, Year, COUNT(*) :: INTEGER AS Awards
+    FROM Summer_Medals
+    WHERE
+        Country IN ('CHN','RUS','USA')
+        AND Year IN (2008, 2012)
+        AND Medal = 'Gold'
+    GROUP BY Country, Year
+    ORDER BY Country ASC, Year ASC;
+$$) AS ct (Country VARCHAR, "2008" INTERGER, "2012" INTEGER)
+
+ORDER BY Country ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Source query
+WITH Country_Awards AS (
+    SELECT
+        Country, Year, COUNT(*) AS Awards
+    FROM Summer_Medals
+    WHERE
+        Country IN ('CHN','RUS','USA')
+        AND Year IN (2004, 2008, 2012)
+        AND Medal = 'Gold' AND Sport = 'Gymnastics'
+    GROUP BY Country, Year
+    ORDER BY Country ASC, Year ASC)
+    
+    SELECT
+        Country, Year,
+        RANK() OVER
+            (PARTITION BY Year ORDER BY Awards DESC) :: INTEGER
+            AS RANK
+        FROM Country_Awards
+        ORDER BY Country ASC, Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Pivot Query
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+...
+$$) AS ct (Country VARCHAR,
+        "2004" INTEGER,
+        "2008" INTEGER,
+        "2012" INTEGER)
+ORDER BY Country ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- A basic pivot:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Create the correct extention to enable CROSSTAB
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  SELECT
+    Gender, Year, Country
+  FROM Summer_Medals
+  WHERE
+    Year IN (2008, 2012)
+    AND Medal = 'Gold'
+    AND Event = 'Pole Vault'
+  ORDER By Gender ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Gender VARCHAR,
+           "2008" VARCHAR,
+           "2012" VARCHAR)
+
+ORDER BY Gender ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Pivoting with ranking:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Count the gold medals per country and year
+SELECT
+  Country,
+  Year,
+  COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+  Country IN ('FRA', 'GBR', 'GER')
+  AND Year IN (2004, 2008, 2012)
+  AND Medal = 'Gold'
+GROUP BY Country, Year
+ORDER BY Country ASC, Year ASC
+-------------------------------------------------------------------------------------------------------------------------------------
+WITH Country_Awards AS (
+  SELECT
+    Country,
+    Year,
+    COUNT(*) AS Awards
+  FROM Summer_Medals
+  WHERE
+    Country IN ('FRA', 'GBR', 'GER')
+    AND Year IN (2004, 2008, 2012)
+    AND Medal = 'Gold'
+  GROUP BY Country, Year)
+
+SELECT
+  -- Select Country and Year
+  Country,
+  Year,
+  -- Rank by gold medals earned per year
+  RANK() OVER
+    (PARTITION BY Year ORDER BY Awards DESC) :: INTEGER AS rank
+FROM Country_Awards
+ORDER BY Country ASC, Year ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+CREATE EXTENSION IF NOT EXISTS tablefunc;
+
+SELECT * FROM CROSSTAB($$
+  WITH Country_Awards AS (
+    SELECT
+      Country,
+      Year,
+      COUNT(*) AS Awards
+    FROM Summer_Medals
+    WHERE
+      Country IN ('FRA', 'GBR', 'GER')
+      AND Year IN (2004, 2008, 2012)
+      AND Medal = 'Gold'
+    GROUP BY Country, Year)
+
+  SELECT
+    Country,
+    Year,
+    RANK() OVER
+      (PARTITION BY Year
+       ORDER BY Awards DESC) :: INTEGER AS rank
+  FROM Country_Awards
+  ORDER BY Country ASC, Year ASC;
+-- Fill in the correct column names for the pivoted table
+$$) AS ct (Country VARCHAR,
+           "2004" INTEGER,
+           "2008" INTEGER,
+           "2012" INTEGER)
+
+Order by Country ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- ROLLUP and CUBE:
+-------------------------------------------------------------------------------------------------------------------------------------
+-- The old way
+SELECT
+    Country, Medal, COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+    Year = 2008 AND COuntry IN ('CHN', 'RUS')
+GROUP BY Country, Medal
+ORDER BY Country ASC, Medal ASC
+
+UNION ALL 
+
+SELECT 
+    COUNTRY, 'Total', COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+    Year = 2008 AND COuntry IN ('CHN', 'RUS')
+GROUP BY Country, 2
+ORDER BY Country ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+-- Enter ROLLUP
+SELECT
+    Country, Metal, COUNT(*) AS Awards
+FROM Summer_Medals
+WHERE
+    Year = 2008 AND Country IN ('CHN', 'RUS')
+GROUP BY Country, ROLLUP(Medal)
+ORDER BY COuntry ASC, Medal ASC;
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------------------------------------------------------------
